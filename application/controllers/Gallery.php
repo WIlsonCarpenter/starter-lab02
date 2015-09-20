cb7c06141ad8c334def26c40a445ab5bbfda9199
Wilson Carpenter <wilsonkcarpenter@gmail.com>
2015-09-19 18:35:54 -0700

    Hooked the database in to updated galleries dynamically

--- application/controllers/Gallery.php
+++ application/controllers/Gallery.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Gallery extends CI_Controller {
+class Gallery extends Application {
 
 	/**
 	 * Index Page for this controller.
@@ -20,8 +20,26 @@ class Gallery extends CI_Controller {
 	 */
 	public function index()
 	{
+		$pix = $this->images->all();
+
+		//build an array of formatted cells
+		foreach ($pix as $picture) {
+			$cells[] = $this->parser->parse("_cell", (array)$picture, true);
+		}
+
+		//prime the table class
+		$this->load->library("table");
+		$parms = array (
+			'table_open' => '<table class="gallery">',
+			'cell_start' => '<td class="oneimage">',
+			'cell_alt_start' => '<td class="oneimage">'
+		);
+		$this->table->set_template($parms);
+
+		$rows = $this->table->make_columns($cells, 3);
+		$this->data['thetable'] = $this->table->generate($rows);
+
 		$this->data['pagebody'] = 'gallery';
 		$this->render();
-		//$this->load->view('gallery');
 	}
 }
